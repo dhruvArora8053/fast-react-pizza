5378b042fb919c744939f677d5d4b5fb07013404
Dhruv Arora <dhruvarora.code@gmail.com>
2024-06-06 14:28:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+
+function App() {
+  return <div>Hello Vite</div>;
+}
+
+export default App;
